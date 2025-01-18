d937e573c61a2d67b0fb682acc6b788af72ec39f
Lucas Giavaroti <lucas.giavaroti@gmail.com>
2025-01-18 15:07:29 -0300

    feat: commits iniciais para o projeto

---
+++ src/main/java/com/lucasgiavaroti/spring_cardapio/SpringCardapioApplication.java
@@ -0,0 +1,13 @@
+package com.lucasgiavaroti.spring_cardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCardapioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCardapioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasgiavaroti/spring_cardapio/SpringCardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasgiavaroti.spring_cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
